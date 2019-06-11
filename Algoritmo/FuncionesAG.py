e1a962c8e50ede19cefb812a177bb4e71210b288
carlos-zrm <carloszrm90@gmail.com>
2019-06-11 11:36:02 -0500

    AG 0.1

--- Algoritmo/FuncionesAG.py
+++ Algoritmo/FuncionesAG.py
@@ -5,9 +5,10 @@ def eval_xx(elementos):
     aptitud = []
     esperanza = []
     sum_total = 0.
+    
     elementos=sorted(elementos)
+    
     print("espacio", elementos)
-    #aptitud = al_eval(espacio_rep)
     aptitud = list(map(lambda x: x**2,elementos))
     print("array aptitud",aptitud)
     sum_total = reduce(lambda a,b : a+b,aptitud)
@@ -18,20 +19,29 @@ def eval_xx(elementos):
     esp_acumulada = [sum(esperanza[0:x+1]) for x in range(0,len(esperanza))]
     print("Esperanza Acumulada", esp_acumulada)
     return elementos, esp_acumulada
-    #return list(map(lambda x: x**2,elementos))
 
 def sel_ruleta(num_eventos, aptitud, esp_acumulada ):
     r = set()
+    c = 0
+    print("\n\n********\n",esp_acumulada)
     while len(r) < num_eventos:
+            # Generar un numero T [0,1)
         prob = np.random.random_sample()
         print("tao",prob)
-        elemento = 0
         #r = list(filter(lambda x: x  > prob, esp_acumulada ))
         aux = [ aptitud[i] for i in range (len(aptitud)) if esp_acumulada[i] >= prob]
-        print(aux)
+        
         saux = set(aux)
         r.update(saux)
         print(r)
+        c+=1
+        if(c> num_eventos**2):
+           print("Tao muy grande")
+           print(prob,esp_acumulada,r)
+           r.update(set(aptitud))
+           break
+    r=sorted(list(r))
+    print("Padres seleccionados",r[len(r)-num_eventos:])
         
     '''
     for i in range(len(esp_acumulada)):
@@ -40,18 +50,33 @@ def sel_ruleta(num_eventos, aptitud, esp_acumulada ):
             break
     return elemento 
     '''
-    return r
-def cruza_un_punto (a,b, longitud):
+    return list(r)[len(r)-num_eventos:]
+def cruza(alg_cruza, padres, longitud=5):
+    aux = []
+    if (len(padres)%2==0):
+        for i in list(range(0,len(padres),2)):
+            aux +=  alg_cruza(padres[i],padres[i+1],longitud)     
+ 
+    else :
+        aux = [padres[i] for i in list(range(0,len(padres)-1,2)) ]
+    #ret = [f]
+    
+    return padres+aux
+def cruza_un_punto (x1,x2, longitud):
     r = np.random.randint(1, longitud  )
     #r = BitArray_gr(int = rand,length=32)
-    r = 3
+    r = 4
     #r1 = BitArray_gr(int = rand,length=32)
     #r2 = BitArray_gr(int = (longitud - rand) ,length=32)
     #print("r",r1.int, r2.int)
+    
+    a = BitArray_gr(int = x1, length=32)
+    b = BitArray_gr(int = x2, length=32)
     aux = BitArray_gr(int = (2**longitud)-1,length=32)
     h1 = BitArray_gr(int = 0,length=32)
     h2 = BitArray_gr(int = 0,length=32)
     
+    #aux = (2**longitud)-1
     #print(aux.int)
 
     aux1 = aux << r
@@ -62,20 +87,27 @@ def cruza_un_punto (a,b, longitud):
     b1 = b & aux2
     h1 = a0 | b1
     h2 = b0 | a1
-    '''
+    
     print(a.bin,b.bin)
     print(a0.bin , a1.bin)
     print(b0.bin , b1.bin)
     print(h1.bin)
     print(h2.bin)
     '''
-    return h1,h2
-    
-def mutacion_prob(a, longitud):
+    print("\n\n Mutacion en un punto", r)
+    print("{0:b}".format(a))
+    print("{0:b}".format(b))
+    print("{0:b}".format(h1))
+    print("{0:b}".format(h2))
+    '''
+    return int(h1.int), int(h2.int)
+    #return x1,x2
+def mutacion_prob(a, longitud=5):
+    a = BitArray(int = a, length=32)
     sub = a.bin[32-longitud:]
-    prob = 1/longitud
+    prob = 1/(2*longitud)
     aux = [ j if np.random.random_sample() >= prob else str(int(not int(j))) for j in sub  ]
     rs = '0'*(32-longitud)
     rs = '0b'+rs+(''.join(aux))
-    return BitArray(rs) 
+    return BitArray(rs).int 
     
--- Algoritmo/interfaz_ag.py
+++ Algoritmo/interfaz_ag.py
@@ -1,6 +1,8 @@
 from Generadora import *
 import numpy as np 
 from FuncionesAG import *
+import matplotlib.pyplot as plt
+
 class interfaz_ag:
     def __init__(self,num_elementos, num_eventos, num_padres = 2, representacion='int' ,alg_eval= eval_xx, alg_sel= sel_ruleta, alg_cruza=cruza_un_punto, alg_mutacion = mutacion_prob):
         self.alg_eval = alg_eval
@@ -10,29 +12,81 @@ class interfaz_ag:
         self.espacio =  Generadora(num_elementos, representacion)
         self.num_padres = num_padres
         self.num_elementos = num_elementos
+        #self.espacio_rep =[]
+
         if representacion is 'int':
             self.espacio_rep = self.espacio.getInt()
         elif representacion is 'float':
             self.espacio_rep = self.espacio.getFloat
-        
+        #self.espacio_rep = self.espacio
 
         #print("Ruleta",self.alg_sel(self.espacio_rep, self.alg_eval))
     def algoritm(self):
+        maximos =[]
+        minimos =[]
         if self.num_elementos % 2 == 0:
-            self.num_eventos = self.num_elementos // 2
+            self.num_padres = self.num_elementos // 2
         else :
-            self.num_eventos = (self.num_elementos // 2)+1
+            self.num_padres = (self.num_elementos // 2)+1
+        '''
+        ******
+            Evaluate P_k
+        ******
+        '''
         aptitud, esp_acumulada = self.alg_eval(self.espacio_rep)
+        #print("eventos",  self.num_eventos )
         
-        r = self.alg_sel(self.num_eventos, aptitud, esp_acumulada)
-        print("r",r)
+        ma = max(aptitud)
+        mi = min(aptitud)
+        maximos.append(ma)
+        minimos.append(mi)
+        
+        '''
+        ******
+            WHile
+        ******
+        '''
+        for i in range(self.num_eventos):
+            padres = self.alg_sel(self.num_padres, aptitud, esp_acumulada)
+            '''
+            ******
+            Crossover
+            ******
+            '''
+            padres = cruza(cruza_un_punto, padres)
+            print(padres)
+            '''
+            ******
+            Mutate
+            ******
+            '''
+            padres = list(map(mutacion_prob,padres))
+            print(padres)
+            aptitud, esp_acumulada = self.alg_eval(padres )
+            ma = max(aptitud)
+            mi = min(aptitud)
+            maximos.append(ma)
+            minimos.append(mi)
+        
+        y = list(range(len(maximos)))
+        fig, ax = plt.subplots()
+        print("listas",self.num_eventos)
+        print(y,maximos)
+        print(y, minimos)
+        ax.plot( maximos)
+        ax.plot(  minimos)
+   
+        #ax.plot( minimos )
+        ax.grid()
+        fig.show()
+        fig.savefig("xoxo.png")
+        #print("r",padres)
         #for i in range(num_eventos):
             
 
-iag = interfaz_ag(4,5)
+iag = interfaz_ag(8,100)
 iag.algoritm()
-l = [0,2,3,4,5]
-print('l',l[])
+
 '''
 a = BitArray_gr(int = 31,length=32)
 b = BitArray_gr(int = 13,length=32)
